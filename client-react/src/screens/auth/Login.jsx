4f75b959dd779d60c5e5d215b41df7003f1f80b4
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2021-06-20 17:50:15 +0500

    authentication & frontend done

--- client-react/src/screens/auth/Login.jsx
+++ client-react/src/screens/auth/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import bgPic from "../../assests/login.jpg"
 import {Link} from "react-router-dom"
+import GoogleLogo from "../../assests/google.svg"
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+
 
 function Login() {
+    	const responseGoogle = (response) => {
+		// sendGoogleToken(response.tokenId);
+	};
+
+	const responseFacebook = (response) => {
+		// sendFacebookToken(response.userID, response.accessToken);
+	};
+    
     return (
       <div class="members-page-wrap">
         <div class="members-content-wrap">
@@ -14,9 +26,44 @@ function Login() {
                 <div class="members-description text-center">
                     Sign into your account for full access
                 </div>
-                <form class="members-form text-left js-member-form" data-members-form="login" autocomplete="off">
+                <form class="members-form text-left js-member-form"  autocomplete="off">
                     <div class="members-form-box">
+                    <GoogleLogin
+									clientId={`${process.env.REACT_APP_GOOGLE_CLIENT}`}
+									onSuccess={responseGoogle}
+									onFailure={responseGoogle}
+									cookiePolicy={'single_host_origin'}
+									render={(renderProps) => (
+										<button
+											onClick={renderProps.onClick}
+											disabled={renderProps.disabled}
+											class="btn btn-block form-field btn-secondary"  style={{margin:"5px 0 10px 0"}} >
+                                            <img className="social-logo" src={GoogleLogo}/>
+											
+												Sign In with Google
+											
+										</button>
+									)}></GoogleLogin>
+								<FacebookLogin
+									appId={`${process.env.REACT_APP_FACEBOOK_CLIENT}`}
+									autoLoad={false}
+									callback={responseFacebook}
+									render={(renderProps) => (
+										<button
+											onClick={renderProps.onClick}
+											class="btn btn-block form-field" style={{margin:"5px 0 20px 0"}} >
+											<div className=' p-2 rounded-full '  >
+												<i className='fab fa-facebook' style={{marginRight:"10px"}} />
+                                                	<span className='ml-4'>
+												Sign In with Facebook
+											</span>
+											</div>
+										
+										</button>
+									)}
+								/>
                         <div class="form-field-wrap">
+
                             <label for="email" class="sr-only">Email</label>
                             <input type="email" class="email form-field" id="email" placeholder="Your email address" required="" autocomplete="off"/>
                                <label for="password" class="sr-only">Password</label>
