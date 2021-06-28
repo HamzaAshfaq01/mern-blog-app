6a143b2e700cae5de23a74cc6bc245d3c7505ecc
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2021-06-28 10:52:32 +0500

    changes done

--- client-react/src/utils/axios.js
+++ client-react/src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 const instance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
-	headers: { 'Content-type': 'application/json' },
+	headers: {
+		'Content-type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+	},
 });
 
 export default instance;
--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use(
 );
 
 app.use(morgan('dev'));
-
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-	next();
-});
+ 
+// app.use(function (req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+// 	next();
+// });
 
 app.use(express.static(path.join(__dirname, './uploads')));
 // Load routes
